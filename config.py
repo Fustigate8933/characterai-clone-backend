de6db9d9434fe38bc191ef8d036c8b30e476a7b7
Fustigate8933 <ianchang891@gmail.com>
2024-10-24 22:46:23 -0400

    update model.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
-IMAGE_EXTENSIONS = [".jpg", ".jpeg", ".png", ".bmp", ".tiff", ".webp"]
 SYSTEM_MESSAGE = {
     "role": "system",
     "content": "You are a helpful assistant.",
 }
-MODEL = "meta-llama/Llama-3.2-11B-Vision-Instruct"
+MODEL = "meta-llama/Llama-3.2-3B-Instruct"
 
